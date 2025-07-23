c48cb8a4d7dd2a177ca032de7578df14dabf14d1
Lucas da Silva Postingher <lucaspostingher@outlook.com>
2025-07-23 19:34:12 -0300

    Remove welcome page and adjuste main route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
 });
 
 Auth::routes();
